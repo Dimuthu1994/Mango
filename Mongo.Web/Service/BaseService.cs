1854b3e714dc4724cc811dd3555e6f34ada86f23
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-11-26 14:26:57 +0530

    Base Service Implementaton

---
+++ Mongo.Web/Service/BaseService.cs
@@ -0,0 +1,80 @@
+﻿using Mango.Web.CouponAPI.Models;
+using Mongo.Web.Models;
+using Mongo.Web.Service.IService;
+using Newtonsoft.Json;
+using System.Text;
+using static Mongo.Web.Utility.SD;
+
+namespace Mongo.Web.Service
+{
+	public class BaseService : IBaseService
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		public BaseService(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
+		{
+			try
+			{
+
+				HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+				HttpRequestMessage message = new();
+				message.Headers.Add("Accept", "application/json");
+				//token
+
+				message.RequestUri = new Uri(requestDto.Url);
+				if (requestDto.Data != null)
+				{
+					message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data), Encoding.UTF8, "application/json");
+				}
+				HttpResponseMessage? apiResponse = null;
+				switch (requestDto.ApiType)
+				{
+					case ApiType.POST:
+						message.Method = HttpMethod.Post;
+						break;
+
+					case ApiType.PUT:
+						message.Method = HttpMethod.Put;
+						break;
+
+					case ApiType.DELETE:
+						message.Method = HttpMethod.Delete;
+						break;
+
+					default:
+						message.Method = HttpMethod.Get;
+						break;
+				}
+				apiResponse = await client.SendAsync(message);
+
+				switch (apiResponse.StatusCode)
+				{
+					case System.Net.HttpStatusCode.NotFound:
+						return new() { IsSuccess = false, Message = "Not Found" };
+					case System.Net.HttpStatusCode.Forbidden:
+						return new() { IsSuccess = false, Message = "Access Denied" };
+					case System.Net.HttpStatusCode.Unauthorized:
+						return new() { IsSuccess = false, Message = "Unauthorized" };
+					case System.Net.HttpStatusCode.InternalServerError:
+						return new() { IsSuccess = false, Message = "Internal Server Error" };
+					default:
+						var apiContent = await apiResponse.Content.ReadAsStringAsync();
+						var apiResposeDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+						return apiResposeDto;
+				}
+			}
+			catch (Exception ex)
+			{
+				var dto = new ResponseDto
+				{
+					Message = ex.Message,
+					IsSuccess = false,
+				};
+				return dto;
+			}
+		}
+	}
+}
---
+++ Mongo.Web/Service/IService/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Web.CouponAPI.Models;
+using Mongo.Web.Models;
+
+namespace Mongo.Web.Service.IService
+{
+	public interface IBaseService
+	{
+		Task<ResponseDto?> SendAsync(RequestDto requestDto);
+	}
+}
