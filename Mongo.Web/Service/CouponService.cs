e4e5647e6b32c297dca42de6a0162df6c4c46add
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-11-27 08:01:55 +0530

    Register Services in Program Class File

---
+++ Mongo.Web/Service/CouponService.cs
@@ -0,0 +1,44 @@
+﻿using Mango.Web.Models;
+using Mongo.Web.Service.IService;
+
+namespace Mongo.Web.Service
+{
+	public class CouponService : ICouponService
+	{
+		private readonly IBaseService _baseService;
+		public CouponService(IBaseService baseService)
+		{
+			_baseService = baseService;
+		}
+
+		public Task<ResponseDto?> CreateCouponsAsync(CouponDto couponCode)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ResponseDto?> DeleteCouponsAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ResponseDto?> GetAllCouponsAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ResponseDto?> GetCouponAsync(string couponCode)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ResponseDto?> GetCouponByIdAsync(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponCode)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
