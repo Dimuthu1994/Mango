64aae4c70d5a1156e1bc85c783eebc8bb6134919
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-11-27 18:15:28 +0530

    create coupon view

--- Mongo.Web/Controllers/CouponController.cs
+++ Mongo.Web/Controllers/CouponController.cs
@@ -5,22 +5,27 @@ using Newtonsoft.Json;
 
 namespace Mongo.Web.Controllers
 {
-    public class CouponController : Controller
-    {
-        private readonly ICouponService _couponService;
-        public CouponController(ICouponService couponService)
-        {
-            _couponService = couponService;
-        }
-        public async Task<IActionResult> CouponIndex()
-        {
-            List<CouponDto>? list = new();
-            ResponseDto? response = await _couponService.GetAllCouponsAsync();
-            if (response != null && response.IsSuccess)
-            {
-                list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
-            }
-            return View(list);
-        }
-    }
+	public class CouponController : Controller
+	{
+		private readonly ICouponService _couponService;
+		public CouponController(ICouponService couponService)
+		{
+			_couponService = couponService;
+		}
+		public async Task<IActionResult> CouponIndex()
+		{
+			List<CouponDto>? list = new();
+			ResponseDto? response = await _couponService.GetAllCouponsAsync();
+			if (response != null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<CouponDto>>(Convert.ToString(response.Result));
+			}
+			return View(list);
+		}
+
+		public async Task<IActionResult> CouponCreate()
+		{
+			return View();
+		}
+	}
 }
