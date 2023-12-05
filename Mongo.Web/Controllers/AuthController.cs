30b0c3f97c48e61e977119c5573f42f30df134a6
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-12-05 16:55:55 +0530

    Auth controller in web project

---
+++ Mongo.Web/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Mango.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Mongo.Web.Service.IService;
+
+namespace Mongo.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
