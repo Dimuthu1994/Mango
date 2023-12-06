ef4f2b63c4787661becae8b8a9eb4e8ddd718ed3
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-12-06 16:29:26 +0530

    Login in Action

--- Mongo.Web/Controllers/AuthController.cs
+++ Mongo.Web/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Mongo.Web.Service.IService;
 using Mongo.Web.Utility;
+using Newtonsoft.Json;
 
 namespace Mongo.Web.Controllers
 {
@@ -20,6 +21,24 @@ namespace Mongo.Web.Controllers
         {
             LoginRequestDto loginRequestDto = new();
             return View(loginRequestDto);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginRequestDto obj)
+        {
+            ResponseDto responseDto = await _authService.LoginAsync(obj);
+
+            if (responseDto != null && responseDto.IsSuccess)
+            {
+                LoginResponseDto loginResponseDto = JsonConvert.DeserializeObject<LoginResponseDto>(Convert.ToString(responseDto.Result));
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", responseDto.Message);
+                return View(obj);
+            }
+
         }
 
         [HttpGet]
