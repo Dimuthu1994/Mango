419a3ebb7860b97312a703603a0595729838f617
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-12-08 07:51:47 +0530

    Passing token to API

--- Mongo.Web/Service/IService/IBaseService.cs
+++ Mongo.Web/Service/IService/IBaseService.cs
@@ -3,9 +3,9 @@ using Mongo.Web.Models;
 
 namespace Mongo.Web.Service.IService
 {
-	public interface IBaseService
-	{
-		//The use of Task in this context indicates that the method is designed to be asynchronous.
-		Task<ResponseDto?> SendAsync(RequestDto requestDto);
-	}
+    public interface IBaseService
+    {
+        //The use of Task in this context indicates that the method is designed to be asynchronous.
+        Task<ResponseDto?> SendAsync(RequestDto requestDto, bool withBearer = true);
+    }
 }
