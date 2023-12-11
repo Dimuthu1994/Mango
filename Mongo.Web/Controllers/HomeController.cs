8ca76ba59850be42f763b110f6a8e33ec67febb1
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-12-11 07:42:28 +0530

    Home controller index Action

--- Mongo.Web/Controllers/HomeController.cs
+++ Mongo.Web/Controllers/HomeController.cs
@@ -1,32 +1,45 @@
+using Mango.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Mongo.Web.Models;
+using Mongo.Web.Service.IService;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mongo.Web.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly IProductService _productService;
+        public HomeController(IProductService productService)
+        {
+            _productService = productService;
+        }
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public async Task<IActionResult> Index()
+        {
+            List<ProductDto>? list = new();
+            ResponseDto? response = await _productService.GetAllProductsAsync();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+            return View(list);
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
