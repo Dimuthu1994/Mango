655b2bc2955911490422147cf63567a39933ca6b
Dimuthu1994 <dimuthudiss6@gmail.com>
2023-12-14 12:56:55 +0530

    Add cartService

---
+++ Mongo.Web/Service/CartService.cs
@@ -0,0 +1,54 @@
+﻿using Mango.Web.Models;
+using Mongo.Web.Service.IService;
+using Mongo.Web.Utility;
+
+namespace Mongo.Web.Service
+{
+    public class CartService : ICartService
+    {
+        private IBaseService _baseService;
+
+        public CartService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> ApplyCouponAsync(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new Models.RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/ApplyCoupon"
+            });
+        }
+
+        public async Task<ResponseDto?> GetCartByUserIdAsync(string userId)
+        {
+            return await _baseService.SendAsync(new Models.RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Url = SD.ShoppingCartAPIBase + "/api/coupon/GetCart/" + userId
+            });
+        }
+
+        public async Task<ResponseDto?> RemoveFromCartAsync(int cartDetailsId)
+        {
+            return await _baseService.SendAsync(new Models.RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDetailsId,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/RemoveCart"
+            });
+        }
+
+        public async Task<ResponseDto?> UpsertCartAsync(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new Models.RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.ShoppingCartAPIBase + "/api/cart/CartUpsert"
+            });
+        }
+    }
+}
